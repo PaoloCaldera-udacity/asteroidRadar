3f832302d116eaec91e3055fec978fde4e5cd806
PaoloCaldera <paolo.caldera@outlook.com>
2023-11-08 17:54:49 +0100

    docs: add wiki folder with screen images
    Take a capture of the two screens characterizing the application and put them in a folder named wiki.

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = BuildConfig.API_KEY
+    const val API_KEY = "3dxf4KwQB5dqKq42yJvMg4BMVIr6VrVftQfmvIpc"
 }
